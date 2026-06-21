8bec85906197a753d5f864beee4153266f5f9b24
Sakeen <Sakeensayaf012@gmai.com>
2026-06-21 21:04:46 +0300

    finished

--- src/executer.cpp
+++ src/executer.cpp
@@ -1,5 +1,5 @@
 #include "executer.hpp"
-
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -7,35 +7,54 @@
 
 #include "builtins.hpp"
 
-void Executer::execute(const std::vector<std::string>& tokens) {
-  if (Builtins::handle(tokens)) {
-    return;
-  }
-
-  std::vector<const char*> argv;
-
-  for (const std::string& token : tokens) {
-    argv.push_back(token.c_str());
-  }
-  argv.push_back(nullptr);
-
-  pid_t pid = fork();
-
-  if (pid < 0) {  // fork failed
-    std::cerr << tokens[0] << ": failed to execute command" << std::endl;
-  } else if (pid == 0) {  // child process
-    int status = execvp(argv[0], const_cast<char* const*>(argv.data()));
-
-    if (status != 0) {
-      std::string msg = "failed to execute command";
-
-      if (errno == ENOENT) {
-        msg = "command not found";
-      }
-
-      std::cerr << tokens[0] << ": " << msg << std::endl;
+void Executer::execute(std::vector<std::string>& tokens) {
+    if (Builtins::handle(tokens)) {
+        return;
     }
-  } else {  // parent process (pid > 0)
-    waitpid(pid, nullptr, 0);
-  }
+
+    std::string outputFile;
+
+    for (size_t i = 0; i < tokens.size(); i++) {
+        if (tokens[i] == ">") {
+            if (i + 1 < tokens.size()) {
+                outputFile = tokens[i + 1];
+                tokens.erase(tokens.begin() + i, tokens.begin() + i + 2);
+            }
+            break;
+        }
+    }
+
+    std::vector<const char*> argv;
+
+    for (const std::string& token : tokens) {
+        argv.push_back(token.c_str());
+    }
+
+    argv.push_back(nullptr);
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        std::cerr << tokens[0] << ": failed to execute command" << std::endl;
+    } else if (pid == 0) {
+        if (!outputFile.empty()) {
+            int file_fd = open(outputFile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            dup2(file_fd, STDOUT_FILENO);
+            close(file_fd);
+        }
+
+        int status = execvp(argv[0], const_cast<char* const*>(argv.data()));
+
+        if (status != 0) {
+            std::string msg = "failed to execute command";
+
+            if (errno == ENOENT) {
+                msg = "command not found";
+            }
+
+            std::cerr << tokens[0] << ": " << msg << std::endl;
+        }
+    } else {
+        waitpid(pid, nullptr, 0);
+    }
 }
